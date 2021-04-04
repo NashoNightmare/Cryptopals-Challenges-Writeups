bf102e63509df09be2f25e44f861cffc27e0bcd4
Nasho Nightmare <44593984+NashoNightmare@users.noreply.github.com>
2021-04-04 15:05:55 +0530

    Update FixedXORER.java

--- Set1/FixedXORER.java
+++ Set1/FixedXORER.java
@@ -1,45 +1,48 @@
-import java.util.Map;
-import java.util.HashMap;
-
-public class hex2base{
+public class FixedXORER{
 	public static void main(String[] args){
-		String hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-		String binary_stream = covertHexToBase64(hex_string);
-		System.out.println(binary_stream);
+		String input1 = "1c0111001f010100061a024b53535009181c";
+		String input2 = "686974207468652062756c6c277320657965";
+		String xored_result = xorer(input1,input2);
+		System.out.println(xored_result);
 	}
 	
-	public static String covertHexToBase64(String hex){
-		
-		// Lookup Table Building - Binary to Base64 Mapping
-		Map<String, String> map = new HashMap<>();
-		String Base64CharSet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-		for(int i=0; i<Base64CharSet1.length(); i++){
-			map.put( Integer.toString(i), Base64CharSet1.charAt(i)+"");
-		}
-		
-		// Converting Hex to Binary
+	public static String hex2bin(String hex){
 		StringBuilder binary = new StringBuilder();
-		for (int i=0; i< hex.length(); i++){
-			int dec = Integer.parseInt(hex.charAt(i) + "" , 16);
-			StringBuilder bin = new StringBuilder(Integer.toBinaryString(dec));
-			while(bin.length() < 4){
-				bin.insert(0, "0");
+		for (int i = 0; i < hex.length() ; i++){
+			
+			int dec = Integer.parseInt(hex.charAt(i)+"", 16);
+			StringBuilder bin = new StringBuilder(Integer.toBinaryString(dec));			
+			while ( bin.length() < 4 ){
+				bin.insert(0,"0");
 			}
 			binary.append(bin);
 		}
-
-		// Converting Binary to Base64 using the Lookup Table
-		StringBuilder b64 = new StringBuilder();
-		for (int i=0; i<binary.length(); i=i+6){
-			String temp = binary.substring(i, i+6);
-			int dec = Integer.parseInt(temp, 2);
-			b64.append((String) map.get(Integer.toString(dec)));
+		return binary.toString();
+	}
+	
+	public static String bin2hex(String bin){
+		String hexChars = "0123456789abcdef";
+		StringBuilder hex = new StringBuilder();
+		for (int i = 0; i < bin.length() ; i=i+4){
+			int dec = Integer.parseInt(bin.substring(i,i+4), 2);
+			hex.append(hexChars.charAt(dec));
 		}
+		return hex.toString();
+	}
+			
+	
+	public static String xorer(String hex1, String hex2){
+		String bin1 = hex2bin(hex1);
+		String bin2 = hex2bin(hex2);
 		
-		return b64.toString();
+		StringBuilder xoredbin = new StringBuilder();
+		
+		for (int i = 0; i < bin1.length() ; i++){
+			int bit1 = Integer.parseInt(bin1.charAt(i)+"");
+			int bit2 = Integer.parseInt(bin2.charAt(i)+"");
+			int xoredbit = bit1 ^ bit2;
+			xoredbin.append(Integer.toString(xoredbit));
+		}
+		return bin2hex(xoredbin.toString());
 	}
 }
-
-
-
-
